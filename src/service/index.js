e6bdd8b3fcc1d68b05cd2212a8015f5ab0828060
Kiranism <iamkiran2831@gmail.com>
2023-08-14 21:45:25 +0530

    env update

--- src/service/index.js
+++ src/service/index.js
@@ -2,7 +2,7 @@ export const CITY_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
 export const geoOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "e35c6fc786msh604f02916866811p164511jsnceceaa14b18a",
+    "X-RapidAPI-Key": `${import.meta.env.VITE_APP_RAPID_API_KEY}`,
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
